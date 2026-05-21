8862173f0981573d1a51b1e0db0cfbe107557ae7
matus.palo <matus.palo@visma.com>
2026-05-21 11:07:13 +0200

    [MP-4] - Stundent implementation

--- src/main/java/gymnazium/sturova/seminarinf/SeminarInfApplication.java
+++ src/main/java/gymnazium/sturova/seminarinf/SeminarInfApplication.java
@@ -1,5 +1,6 @@
 package gymnazium.sturova.seminarinf;
 
+import gymnazium.sturova.seminarinf.dto.StudentDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,17 @@ public class SeminarInfApplication {
     public static void main(String[] args) {
         SpringApplication.run(SeminarInfApplication.class, args);
         System.out.println("Application started successfully.");
+
+        StudentDTO student = new StudentDTO();
+        student.setId(1);
+        student.setSurname("Novak");
+        student.setName("Jan");
+        student.setEmail("fdgj");
+        student.setPhoneNumber("123456789");
+        student.setAge(20);
+
+        System.out.println(student);
+
     }
 
 }
